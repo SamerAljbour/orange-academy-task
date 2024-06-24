bc396753f56a03184a0da4d73d55dfc1cd106889
SamerAljbour <samerseran34@gmail.com>
2024-06-24 20:12:41 +0300

    weather api

---
+++ javaScript/api/weather-ready/app.js
@@ -0,0 +1,44 @@
+let citySelect = document.querySelector(".location-button")
+let pressure = document.querySelector(".PRESSURE")
+let humidityView = document.querySelector(".humidity-view")
+let windView = document.querySelector(".wind-view")
+let weatherTemp = document.querySelector(".weather-temp")
+let weatherDesc = document.querySelector(".weather-desc")
+let country = document.querySelector(".location")
+let showDate = document.querySelector(".date-day")
+let dateDayName = document.querySelector(".date-dayname")
+let ammanCity = document.getElementById("amman")
+let parisCity = document.getElementById("paris")
+let londonCity = document.getElementById("london")
+var cityValue = "Paris";
+const months = ["Jan", "Febr", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let date = new Date()
+let day = date.getDate(), month = months[date.getMonth()], year = date.getFullYear(), dayName = days[date.getDay()]
+citySelect.addEventListener("change", () => {
+    cityValue = citySelect.value
+    fetchCityWeather(cityValue)
+
+})
+
+function fetchCityWeather(cityValue) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=cceec6b17d334a5f70ea1c49b3f30e91&units=metric`)
+        .then(response => {
+            if (!response.ok)
+                throw Error("status error code :" + response.status)
+            return response.json()
+        }).then(data => {
+            pressure.textContent = data.main.pressure + "%"
+            humidityView.textContent = data.main.humidity + "%"
+            windView.textContent = data.wind.speed + "km/h"
+            weatherTemp.textContent = parseInt(data.main.temp) + "°C"
+            weatherDesc.textContent = data.weather[0].description
+            country.textContent = `${cityValue} ,${data.sys.country}`
+            showDate.textContent = `${day} ${month} ${year}`
+            dateDayName.textContent = `${dayName}`
+        })
+
+}
+window.onload = () => {
+    fetchCityWeather(cityValue);
+}
